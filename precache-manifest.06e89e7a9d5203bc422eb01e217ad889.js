75029f40183b14139ded4f7e14fad5f670d9c4a8
wes-chen <35469114+wes-chen@users.noreply.github.com>
2019-03-28 13:12:54 -0700

    Updates

---
+++ precache-manifest.06e89e7a9d5203bc422eb01e217ad889.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b9958bf6a6bcd4d96cc",
+    "url": "/totally-real-iq-test/static/css/main.25fc9631.chunk.css"
+  },
+  {
+    "revision": "1b9958bf6a6bcd4d96cc",
+    "url": "/totally-real-iq-test/static/js/main.3703448c.chunk.js"
+  },
+  {
+    "revision": "ab81c93f306b6cc86363",
+    "url": "/totally-real-iq-test/static/js/runtime~main.29e0e0e5.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/totally-real-iq-test/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/totally-real-iq-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1847f090c6acb730672c0b3c9d1e42a3",
+    "url": "/totally-real-iq-test/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/totally-real-iq-test/precache-manifest.06e89e7a9d5203bc422eb01e217ad889.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/totally-real-iq-test/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
